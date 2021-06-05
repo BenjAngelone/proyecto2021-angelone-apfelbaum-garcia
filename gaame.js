37a4db7de9974f3e98c9696fa2aade123d8db736
BenjAngelone <2008273@ucc.edu.ar>
2021-06-05 16:13:35 -0300

    el  huevo se arrastra  con  el cursor

--- gaame.js
+++ gaame.js
@@ -1,4 +1,4 @@
-console.log("holaa gil aca estoy" );
+console.log("holaa  aca estoy" );
 
 
 var canvas = document.getElementById("myCanvas");
@@ -11,15 +11,10 @@ canvas.addEventListener("mousemove", function(e) {
     var canvasY = Math.round(e.clientY - cRect.top);   // from the X/Y positions to make  
     ctx.clearRect(0, 0, canvas.width, canvas.height);  // (0,0) the top left of the canvas
     ctx.fillText("X: "+canvasX+", Y: "+canvasY, 10, 20);
-
+    var img = document.getElementById("drag-items");
+    ctx.drawImage(img, canvasX, canvasY,90, 90);
 });
 
 
-var image=new Image();
-image.onload=function(){
-context.drawImage(image,0,0,canvas.width,canvas.height);
-};
-image.src="huevo  1.pmg";
 
-
-
+  
