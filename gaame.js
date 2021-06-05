99a843f6fed6d7fe34bdc5c8fa596e6987396d22
BenjAngelone <2008273@ucc.edu.ar>
2021-06-05 18:05:51 -0300

    intento de agarrar el huevo con una  bandera

--- gaame.js
+++ gaame.js
@@ -5,16 +5,21 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 ctx.font = "16px Arial";
 
-canvas.addEventListener("mousemove", function(e) { 
-    var cRect = canvas.getBoundingClientRect();        // Gets CSS pos, and width/height
-    var canvasX = Math.round(e.clientX - cRect.left);  // Subtract the 'left' of the canvas 
-    var canvasY = Math.round(e.clientY - cRect.top);   // from the X/Y positions to make  
-    ctx.clearRect(0, 0, canvas.width, canvas.height);  // (0,0) the top left of the canvas
-    ctx.fillText("X: "+canvasX+", Y: "+canvasY, 10, 20);
-    var img = document.getElementById("drag-items");
-    ctx.drawImage(img, canvasX, canvasY,90, 90);
-});
+var  agarrado=0;
+canvas.onmousedown = function (e){agarrado=1;}
+canvas.onmouseup = function(e){agarrado=0}
 
+if(agarrado==1){
+    canvas.addEventListener("mousemove", function(e) { 
+        var cRect = canvas.getBoundingClientRect();        // Gets CSS pos, and width/height
+        var canvasX = Math.round(e.clientX - cRect.left);  // Subtract the 'left' of the canvas 
+        var canvasY = Math.round(e.clientY - cRect.top);   // from the X/Y positions to make  
+        ctx.clearRect(0, 0, canvas.width, canvas.height);  // (0,0) the top left of the canvas
+        ctx.fillText("X: "+canvasX+", Y: "+canvasY, 10, 20);
+        var img = document.getElementById("drag-items");
+        ctx.drawImage(img, canvasX, canvasY,90, 90);
+    });
+}
 
 
   
